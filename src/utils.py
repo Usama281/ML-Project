31c16c98bcf45bbda3b54f6cc3a57246e4375df0
Usama Afzal <usamafzal36@gmail.com>
2024-08-02 19:45:04 +0500

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -21,6 +21,14 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_object:
+            return pickle.load(file_object)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
 def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
